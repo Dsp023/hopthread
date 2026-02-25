3e876719143fbc0ee77dfc566dfec2da249468d2
Devi Sri Prasad <nakkadevisriprasad@gmail.com>
2026-02-25 17:57:45 +0000

    feat: Add Context Condenser V2 (Memory Graph)

---
+++ src/features/context-condenser-v2.ts
@@ -0,0 +1,6 @@
+// Context Condenser V2 (Memory Graph)
+export class ContextCondenserV2 {
+  public condense(context: string[]): string {
+    return `Condensed context: ${context.join(' ')}`;
+  }
+}
